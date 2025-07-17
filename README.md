4a1ac1c1147a8b223071911610b5d3a59dca7b68
ceraskotin <ceraskotin@gmail.com>
2025-07-17 14:32:19 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# viralbo
-viralbo
+# [viralbo](https://viralbo.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Viral Bo';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://viralbo.pages.dev';
+export const terbit = '2025-01-02T00:01:03Z';
+
+export const google = 'WM6BwFw8T4Dz9A9R7oEVHNX_k-95VnTSXuKMI4HBQv8';
+export const yandex = 'da4800c60b92c6ae'
+
+export const index = '08fc74ac-d7b1-4b54-8866-4f8844bb7403';
